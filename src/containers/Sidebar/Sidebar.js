bf66d11962a0b175494a5bfe8e1e60463567146b
Zbeb <zbeb@me.com>
2020-07-16 14:37:45 +0200

    Basic structure

---
+++ src/containers/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Sidebar extends React.Component {
+
+    constructor() {
+        this.RSSFeed = "";
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
