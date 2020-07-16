55a0307cacf1f2fe23f703c96c67a58032fa3125
Zbeb <zbeb@me.com>
2020-07-16 11:20:37 +0200

    Updates

---
+++ precache-manifest.d90a90a104a204cc5a7ea57f81b71f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc76fedeb938600b47fd195a727fc00",
+    "url": "/react-rss-feed/index.html"
+  },
+  {
+    "revision": "0daec74c6d65542c6195",
+    "url": "/react-rss-feed/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05179b43fd3d5e6122a5",
+    "url": "/react-rss-feed/static/js/2.e4d2b15e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rss-feed/static/js/2.e4d2b15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0daec74c6d65542c6195",
+    "url": "/react-rss-feed/static/js/main.be9745a8.chunk.js"
+  },
+  {
+    "revision": "dacc403214b1d242b8af",
+    "url": "/react-rss-feed/static/js/runtime-main.ae9127e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-rss-feed/static/media/logo.5d5d9eef.svg"
+  }
+]);
