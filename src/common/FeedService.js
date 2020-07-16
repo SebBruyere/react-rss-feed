fa2e6feda099da5805591dca4e20bcbb3e9f860c
Zbeb <zbeb@me.com>
2020-07-16 16:24:50 +0200

    Fetching RSS feed through rss2json api

---
+++ src/common/FeedService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default class FeedService {
+
+    constructor() {
+        this.RSSFeedTest = `https://www.lemonde.fr/rss/une.xml`;
+        this.APIKey = `xe3tuhc9olqsuhvn9fc8fzgtmrgdjcqnl514pxqz`;
+        this.baseURL = `https://api.rss2json.com/v1/api.json?rss_url=`;
+    }
+
+    fetchRSS(callBack) {
+        axios.get(`${this.baseURL}${this.RSSFeedTest}&api_key=${this.APIKey}`)
+            .then(res => {
+                callBack(res);
+            });
+    }
+}
--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,16 +1,29 @@
 import React from "react";
+import FeedService from "./../../common/FeedService";
+
+const feedService = new FeedService();
 
 export default class Sidebar extends React.Component {
 
-    constructor() {
-        this.RSSFeed = "";
+    constructor () {
+        super ();
+
+        this.RSSFeed = `https://www.lemonde.fr/rss/une.xml`;
+        this.RSSData = "";
+    }
+
+    componentDidMount () {
+        feedService.fetchRSS(result => {
+            this.setRSSData = result;
+        });
     }
 
     render () {
+
         return (
             <div>
                 <h1>Hello</h1>
             </div>
-        )
+        );
     }
 }
