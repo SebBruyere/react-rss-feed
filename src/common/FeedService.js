edfd7e7239f41b6d6a4c746300c8a8aa5edf2f41
Zbeb <zbeb@me.com>
2020-07-20 00:17:11 +0200

    Adding proxy to prevent CORS fetch

--- src/common/FeedService.js
+++ src/common/FeedService.js
@@ -4,10 +4,11 @@ export default class FeedService {
         this.RSSFeedTest = `https://www.lemonde.fr/rss/une.xml`;
         this.APIKey = `xe3tuhc9olqsuhvn9fc8fzgtmrgdjcqnl514pxqz`;
         this.baseURL = `https://api.rss2json.com/v1/api.json?rss_url=`;
+        this.proxyURL = `https://cors-anywhere.herokuapp.com/`;
     }
 
     fetchRSS(callBack) {
-        fetch(this.RSSFeedTest)
+        fetch(this.proxyURL + this.RSSFeedTest)
             .then(response => response.text())
             .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
             .then(data => callBack( this.getItemChildNodes(data) ));
@@ -15,10 +16,10 @@ export default class FeedService {
 
     getItemChildNodes(RSSData) {
 
+        console.log(RSSData);
+
         var filteredItem = [];
         var items = RSSData.getElementsByTagName("item");
-
-        console.log(items);
 
         // Retrieve all tag names of this DOM Node
         var parentTags = this.getTags(items[0]);
@@ -32,12 +33,6 @@ export default class FeedService {
                 // Protect from empty tag text content
                 if (currentItem) {
                     if (currentItem.childElementCount) {
-                        // var childTags = this.getTags(currentItem);
-                        // var childObj = {};
-                        // childTags.map( childTag => {
-                        //     console.log(currentItem);
-                        //     childObj[childTag] = currentItem.getElementsByTagName(childTag)[0]
-                        // });
                         if (currentItem.hasAttribute("url")) {
                             if (currentItem.prefix) {
                                 itemObject[currentItem.prefix] = currentItem.getAttribute("url");
